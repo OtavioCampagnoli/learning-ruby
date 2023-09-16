d91e887d4a5e889810d7674e4ad5f486d741023e
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2023-09-15 22:02:23 -0300

    basic-sytaxe: Variables

---
+++ 01-basic-sytaxe/variables.rb
@@ -0,0 +1,13 @@
+#Dynamic type => If I'm using the same variable at the same time execution, the value of him will be changed.
+
+message = "Otavio"
+puts message.reverse!
+message = 1.56
+puts message = 90
+puts message
+
+name = "Henrique"
+age = 20
+height = 1.90
+
+puts name, age, height
